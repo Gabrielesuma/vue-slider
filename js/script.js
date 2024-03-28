100e6b00bba774b027e0483f5da01ed91383de83
Gabrielesuma <gabrielesuma139@gmail.com>
2024-03-28 16:07:03 +0100

    setInterval

---
+++ js/script.js
@@ -0,0 +1,31 @@
+import {slides} from './data.js';
+
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            slides: slides,
+            activeIndexSlide: 0
+        }
+    },
+    methods: {
+        nextSlide(){
+            if(this.activeIndexSlide < this.slides.length - 1){
+                this.activeIndexSlide++;
+            } else {
+                this.activeIndexSlide = 0;
+            }
+        },
+        prevSlide(){
+            if(this.activeIndexSlide > 0){
+                this.activeIndexSlide--;
+            } else {
+                this.activeIndexSlide = this.slides.length - 1;
+            }
+        }
+    },
+    mounted(){
+        setInterval(this.nextSlide, 3000)
+    }
+}).mount('#app');
